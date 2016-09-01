b99e8f365d221fc85ceb0f88293bff1c19ef0f71
Sam Win-Mason <sam@albedogames.com>
2016-09-01 17:02:48 +1200

    DB Relations

--- src/MPWebAPI/Models/PostgresDBContext.cs
+++ src/MPWebAPI/Models/PostgresDBContext.cs
@@ -11,33 +11,95 @@ namespace MPWebAPI.Models
         {
         }
 
-        // public DbSet<TodoList> TodoLists { get; set; }
-        // public DbSet<TodoItem> TodoItems { get; set; }
+        public DbSet<Alignment> Alignments;
+        
+        public DbSet<BusinessUnit> BusinessUnits;
+        
+        public DbSet<BenefitCategory> BenefitCategories;
+        
+        public DbSet<StaffResourceCategory> StaffResourceCategories;
+        
+        public DbSet<FinancialResourceCategory> FinancialResourceCategories;
+        
+        public DbSet<RiskCategory> RiskCategories;
+        
+        public DbSet<AlignmentCategory> AlignmentCategories;
+        
+        public DbSet<FinancialAdjustment> FinancialAdjustments;
+        
+        public DbSet<FinancialResource> FinancialResources;
+        
+        public DbSet<FinancialResourcePartition> FinancialResourcePartitions;
+        public DbSet<PartitionResourceCategory> PartitionResourceCategories;
+        
+        public DbSet<FinancialTransactionResourceCategory> FinancialTransactionResourceCategories;
+        public DbSet<FinancialTransaction> FinancialTransactions;
+        
+        public DbSet<Group> Groups;
+        public DbSet<UserGroup> UserGroups;
+        
+        public DbSet<Organisation> Organisations;
 
-        // protected override void OnModelCreating(ModelBuilder builder)
-        // {
-        //     // relations
-        //     builder.Entity<TodoList>().HasKey(l => l.TodoListId);
-        //     builder.Entity<TodoList>()
-        //         .HasMany(l => l.Items)
-        //         .WithOne();
+        public DbSet<PhaseConfig> PhaseConfigs;
+        
+        public DbSet<Plan> Plans;
+        public DbSet<PlanUser> PlanUsers;
 
-        //     // property constraints
-        //     builder.Entity<TodoList>()
-        //         .Property(l => l.Name)
-        //         .HasMaxLength(250)
-        //         .HasDefaultValue("New List");
+        public DbSet<Project> Projects;
+        public DbSet<StaffResourceProject> StaffResourceProjects;
+        public DbSet<ProjectFinancialResourceCategory> ProjectFinancialResourceCategories;
+        public DbSet<ProjectUser> ProjectUsers;
+
+        public DbSet<ProjectBenefit> ProjectBenefits;
+        public DbSet<ProjectBenefitBenefitCategory> ProjectBenefitBenefitCategories;
+
+        public DbSet<ProjectConfig> ProjectConfigs;
+
+        public DbSet<ProjectOption> ProjectOptions;
+        public DbSet<ProjectDependency> ProjectDependencies;
+
+        public DbSet<ProjectPhase> ProjectPhases;
+
+        public DbSet<ResourceScenario> ResourceScenarios;
+        public DbSet<ResourceScenarioUser> ResourceScenarioUsers;
+
+        public DbSet<RiskProfile> RiskProfiles;
+
+        public DbSet<StaffResource> StaffResources;
+        public DbSet<StaffResourceStaffResourceCategory> StaffResourceStaffResourceCategories;
+
+        public DbSet<StaffTransaction> StaffTransactions;
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            // relations
+
+            // Alignment
+            builder.Entity<Alignment>()
+                .HasOne(a => a.AlignmentCategory)
+                .WithMany(ac => ac.Alignments)
+                .HasForeignKey(a => a.AlignmentCategoryId);
             
-        //     builder.Entity<TodoItem>()
-        //         .Property(i => i.ItemText)
-        //         .HasMaxLength(500)
-        //         .HasDefaultValue("New Item");
+            builder.Entity<Alignment>()
+                .HasOne(a => a.ProjectBenefit)
+                .WithMany(pb => pb.Alignments)
+                .HasForeignKey(a => a.ProjectBenefitId);
 
-        //     builder.Entity<TodoItem>()
-        //         .Property(i => i.Completed)
-        //         .HasDefaultValue(false);
+            // BusinessUnit
+            builder.Entity<BusinessUnit>()
+                .HasOne(bu => bu.Organisation)
+                .WithMany(o => o.BusinessUnits)
+                .HasForeignKey(bu => bu.OrganisationId);
+            
+            // FinancialResourceCategory
+            builder.Entity<FinancialResourceCategory>()
+                .HasOne(frc => frc.Group)
+                .WithMany(g => g.FinancialResourceCategories)
+                .HasForeignKey(frc => frc.GroupId);
+                
 
-        //     base.OnModelCreating(builder);
-        // }
+        
+            base.OnModelCreating(builder);
+        }
     }
 }
