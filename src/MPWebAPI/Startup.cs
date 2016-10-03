4b906faa3402842f17ec0ae913586063068e9b7e
Sam Win-Mason <sam@albedogames.com>
2016-10-04 10:44:37 +1300

    Moved db connection string to usersecrets file

--- src/MPWebAPI/Startup.cs
+++ src/MPWebAPI/Startup.cs
@@ -34,8 +34,8 @@ namespace MPWebAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            var sqlConnectionString = Configuration["DevDB:ConnectionString"];
-
+            var sqlConnectionString = Configuration["DevDBConnectionString"];
+            
             services.AddOptions();
             services.Configure<MerlinPlanBLOptions>(
                 Configuration.GetSection("BusinessRules"));
