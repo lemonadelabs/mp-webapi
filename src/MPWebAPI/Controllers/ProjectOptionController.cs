f648b3f1bf79d972178f8c09c445a69be28591f3
sam <sam@albedosoftware.com>
2016-11-22 16:15:13 +1300

    Added get all project options endpoint

---
+++ src/MPWebAPI/Controllers/ProjectOptionController.cs
@@ -0,0 +1,26 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using MPWebAPI.Models;
+using MPWebAPI.ViewModels;
+
+namespace MPWebAPI.Controllers
+{
+    [Route("api/[Controller]")]
+    public class ProjectOptionController : Controller
+    {
+
+        private readonly IMerlinPlanRepository _repository;
+        private readonly IMerlinPlanBL _businessLogic;
+
+        public ProjectOptionController(IMerlinPlanBL mpbl, IMerlinPlanRepository repo)
+        {
+            _repository = repo;
+            _businessLogic = mpbl;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll() => Ok(_repository.ProjectOptions.Select(po => new ProjectOptionViewModel(po)));
+
+
+    }
+}
